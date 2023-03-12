36a073416099ad1e34d241f6e9c33ccd8d1a5207
Toniskeva <tskeva@pmfst.hr>
2023-03-12 15:07:06 +0100

    Zadatak 4

---
+++ Vjezbe/Vjezbe_1/Zadatak 4.py
@@ -0,0 +1,18 @@
+def funkcija(x1,y1,x2,y2):
+    t1=(x1,y1)
+    t2=(x2,y2)
+    if not t1==t2:
+        k=(y2 - y1)/(x2 - x1)
+        n=-k*x1+y1
+        if n<0:
+            y= str(k)+'x'+str(n)
+            print(y)
+        if n>0:
+            y=str(k)+'x'+'+'+str(n)
+            print(y)
+        if n==0:
+            y=str(k)+'x'
+            print(y)
+    else:
+        print('Unijeli ste koordinate istih točaka, unesite ponovno.')
+funkcija(2,2,3,5)
