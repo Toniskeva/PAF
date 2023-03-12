535448f7f670c8cb5bce48bee2f7cbbbfecf2e1b
Toniskeva <tskeva@pmfst.hr>
2023-03-12 15:07:19 +0100

    Zadatak 5

---
+++ Vjezbe/Vjezbe_1/Zadatak 5.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def funkcija(x1,y1,x2,y2):
+    t1=(x1,y1)
+    t2=(x2,y2)
+    if not t1==t2:
+        k=(y2 - y1)/(x2 - x1)
+        n=-k*x1+y1
+        if n<0:
+            y= str(k)+'x'+str(n)
+            print(y)
+        if n>0:
+            y=str(k)+'x'+'+'+str(n)
+            print(y)
+        if n==0:
+            y=str(k)+'x'
+            print(y)
+    a=float(input('Unesite minimalnu vrijednost na x osi: '))
+    b=float(input('Uneite maksimalnu vrijednot na x osi: '))
+    x=np.arange(a,b,1)
+    y=k*x+n
+    print(y)
+    fig=plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    axes.set_xlabel('x')
+    axes.set_ylabel('y')
+    axes.plot(x,y)
+    z=int(input('Za ispis grafa na ekranu uneisite 0, za spremanje grafa u obliku PDF-a uneite 1: '))
+    if z==0:
+        plt.show()
+    elif z==1:
+        ime=input('Uneiste ime slike: ')
+        i=ime+'.png'
+        fig.savefig(i)
+    if t1==t2:
+        print('Unijeli ste koordinate istih točaka, unesite ponovno.')
+funkcija(2,2,3,10)
+
