02972b996eac5ec147b159103b66a9883ef4ce6f
Toniskeva <tskeva@pmfst.hr>
2023-03-16 23:41:23 +0100

    Unos točke ispravak

--- Vjezbe/Vjezbe_1/Zadatak 5.py
+++ Vjezbe/Vjezbe_1/Zadatak 5.py
@@ -16,24 +16,24 @@ def funkcija(t1,t2):
         if n==0:
             y=str(k)+'x'
             print(y)
-    a=float(input('Unesite minimalnu vrijednost na x osi: '))
-    b=float(input('Uneite maksimalnu vrijednot na x osi: '))
-    x=np.arange(a,b,1)
-    y3=k*x+n
-    fig=plt.figure()
-    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-    axes.set_xlabel('x')
-    axes.set_ylabel('y')
-    axes.plot(x1,y1,'b.')
-    axes.plot(x2,y2,'b.')
-    axes.plot(x,y3)
-    z=int(input('Za ispis grafa na ekranu uneisite 0, za spremanje grafa u obliku PDF-a uneite 1: '))
-    if z==0:
-        plt.show()
-    elif z==1:
-        ime=input('Uneiste ime slike: ')
-        i=ime+'.png'
-        fig.savefig(i)
+        a=float(input('Unesite minimalnu vrijednost na x osi: '))
+        b=float(input('Uneite maksimalnu vrijednot na x osi: '))
+        x=np.arange(a,b,1)
+        y3=k*x+n
+        fig=plt.figure()
+        axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+        axes.set_xlabel('x')
+        axes.set_ylabel('y')
+        axes.plot(x1,y1,'b.')
+        axes.plot(x2,y2,'b.')
+        axes.plot(x,y3)
+        z=int(input('Za ispis grafa na ekranu uneisite 0, za spremanje grafa u obliku PDF-a uneite 1: '))
+        if z==0:
+            plt.show()
+        elif z==1:
+            ime=input('Uneiste ime slike: ')
+            i=ime+'.png'
+            fig.savefig(i)
     if t1==t2:
         print('Unijeli ste koordinate istih točaka, unesite ponovno.')
-funkcija((0,5),(2,10))
+funkcija((2,4),(4,10))
