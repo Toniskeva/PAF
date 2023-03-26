7df7863455031246375e94b297380f6746cc3176
Toniskeva <tskeva@pmfst.hr>
2023-03-26 20:11:50 +0200

    Vjezbe 3

--- Vjezbe/Vjezbe_3/Zadatak 2.py
+++ Vjezbe/Vjezbe_3/Zadatak 2.py
@@ -8,4 +8,5 @@ def funkcija(N):
 funkcija(200)
 funkcija(2000)
 funkcija(20000)
-#Pri povećavanju broja iteracija,povećavanjem broja S sve je manja razlika između brojeva pri zbrajanju i oduzimanju jer drugi se drugi broj zaokružuje sve više kako je 5 značajnije od male decimale te je sve manji
+#Pri povećavanju broja iteracija,sve više se povećava broj S, pa je svakom vrtnjom petlje oduzimanja sve manja razlika između broja S
+#i novog broja S nakon oduzimanja 1/3, pa se odbacuju decimale koje su neznatne u odnosu na velik cijeli dio broja i time nastaje sve veći konačni ostatak zbog zaokruživanja pomoću baze 2.
