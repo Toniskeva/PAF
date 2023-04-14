75db9d97a3281a60c8b4d96c507b3ba024875685
Toniskeva <tskeva@pmfst.hr>
2023-04-14 04:46:33 +0200

    Vježbe 4

--- Vjezbe/Vjezbe_4/Zadatak 2.py
+++ Vjezbe/Vjezbe_4/Zadatak 2.py
@@ -6,6 +6,7 @@ import particle as particle
 p1=particle.Particle(10,60,0,0)
 g=9.81
 D=2*p1.v*np.cos(p1.k)*math.sqrt((p1.v*np.sin(p1.k))**2/g**2)
+print(D)
 lista_greska=[]
 for dt in np.linspace(0.01,0.1,1000):
     n=D-p1.range(dt)/D*100
