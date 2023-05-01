0488e62d629190b94f63aee54e7160da10448496
Toniskeva <tskeva@pmfst.hr>
2023-05-01 03:35:05 +0200

    Vježbe 6

--- Vjezbe/Vjezbe_6/harmonic_oscillator_test.py
+++ Vjezbe/Vjezbe_6/harmonic_oscillator_test.py
@@ -7,7 +7,7 @@ vrijeme_period=[0.01,0.1,0.2,0.3]
 p1=ho.HarmonicOscillator(2,2,10,0)
 for i in vrijeme_trajectory:
     p1.plot_trajectory(0,10,i)
-for i in vrijeme_period: #Ne mora nužno konvergirat period s jedne strane svakim korakom prema analitičkom periodu jer je postoji greška u razmaku između mijenjanja 2 predznaka
+for i in vrijeme_period: #Ne mora nužno konvergirat period s jedne strane svakim korakom prema analitičkom periodu jer postoji greška u razmaku između mijenjanja 2 predznaka
     p1.graf_period(i)
 plt.xlabel('T[s]')
 plt.ylabel('A[m]')
