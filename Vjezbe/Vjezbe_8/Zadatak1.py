3f455831f00cada26d22bcb72a9d3beaeb8a14f5
Toniskeva <tskeva@pmfst.hr>
2023-05-12 03:18:39 +0200

    Vježbe 8

--- Vjezbe/Vjezbe_8/Zadatak1.py
+++ Vjezbe/Vjezbe_8/Zadatak1.py
@@ -2,13 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class particle:
-    def __init__(self,v,m,q):
+    def __init__(self,v,m,q,x0):
         self.v=v
         self.m=m
         self.q=q
-        self.x=(0,0,0)
-        self.pocetni=v,m
-    def gibanje(self,E,B,dt):
+        self.x=x0
+        self.pocetni=v,m,q,x0
+    def reset(self):
+        self.v,self.m,self.q,self.x=self.pocetni
+    def __move(self,E,B,dt):
         self.a=(self.q*(np.array(E)+np.cross(np.array(self.v),np.array(B))))/self.m
         self.v=self.v+self.a*dt
         self.x=self.x+self.v*dt
@@ -17,7 +19,7 @@ class particle:
         lista_y=[]
         lista_z=[]
         while a<b:
-            self.gibanje(E,B,dt)
+            self.__move(E,B,dt)
             lista_x.append(self.x[0])
             lista_y.append(self.x[1])
             lista_z.append(self.x[2])
@@ -29,9 +31,10 @@ class particle:
         zline=lista_z
         ax.plot3D(xline,yline,zline,'blue',label='{}'.format(cestica))
         plt.legend()
+        self.reset()
 
-p1=particle((1,1,1),1,1)
-p2=particle((1,1,1),1,-1)
+p1=particle((1,1,1),1,1,(0,0,0))
+p2=particle((1,1,1),1,-1,(0,0,0))
 p1.plot_trajectory(1,25,0.001,(0,0,0),(0,0,1),'Pozitron')
 p2.plot_trajectory(1,25,0.001,(0,0,0),(0,0,1),'Elektron')
 plt.show()
